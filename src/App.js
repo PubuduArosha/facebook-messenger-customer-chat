f787b878e98c18ce3255c428d9d584052a15f92f
Pubudu Arosha <it18108514@my.sliit.lk>
2021-04-23 02:58:55 +0530

    compeled

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import logo from './logo.svg';
+import logo from './technomic6.png';
 import './App.css';
 
 import MessengerCustomerChat from 'react-messenger-customer-chat';
@@ -10,17 +10,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
       <MessengerCustomerChat pageId="101249441233215" appId="3664419150244134"/>,
     </div>
